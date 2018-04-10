e1e9c68231ccfa804c48473cb577d8a92ee07d14
Mark Cronin <mark_cronin@icloud.com>
2018-04-10 13:01:31 +1000

    added isFizzBuzz_4_7

--- FizzBuzz_wFunction.js
+++ FizzBuzz_wFunction.js
@@ -1,4 +1,4 @@
-function isFizzBuzz (x){
+function isFizzBuzz_3_5 (x){
   if ( x % 15 === 0){
     return "FIZZBUZZ"
   } else if ( x % 5 === 0){
@@ -10,6 +10,18 @@ function isFizzBuzz (x){
   }
 }
 
+function isFizzBuzz_4_7 (x){
+  if ( x % 28 === 0){
+    return "FIZZBUZZ"
+  } else if ( x % 7 === 0){
+    return "BUZZ"
+  } else if ( x % 4 === 0){
+    return "FIZZ"
+  } else {
+    return x
+  }
+}
+
 for (x = 1; x <= 100; x++){
-  console.log (isFizzBuzz(x))
+  console.log (isFizzBuzz_4_7(x))
 }
