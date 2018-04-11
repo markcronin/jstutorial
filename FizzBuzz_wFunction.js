d704e8638776ea8fd8406e0e6dc0b7e461a25fa8
Mark Cronin <mark_cronin@icloud.com>
2018-04-11 10:52:52 +1000

    Is this right?
    I think I've done it, but it just feels like I've made it more complicatwed without adding any additional functionallity

--- FizzBuzz_wFunction.js
+++ FizzBuzz_wFunction.js
@@ -39,12 +39,46 @@ function isFizzBuzz_x_y(x,num1,num2){
   }
 }
 
-function makeFizzBuzzFunc (num1,num2){
-  var extraStep = function(x){
-    //stuff goes here?? 
+// prints FIZZBUZZ between a given range (lower to upper)
+// prints FIZZ for any number divisible by num1
+// prints BUZZ for any number divisible by num2
+// prints FIZZBUZZ for any number divisible by both num1 and num2
+
+function isFizzBuzz_range(lower,upper,num1,num2){
+  for (let x = lower; x <= upper; x++){
+    if (x % (num1 * num2) === 0){
+      console.log ("FIZZBUZ");
+    } else if ( x % num2 === 0){
+      console.log ("BUZZ");
+    } else if ( x % num1 === 0){
+      console.log ("FIZZ");
+    } else {
+      console.log (x);
+    }
   }
+  return "";
 }
 
-for (x = 1; x <= 100; x++){
-  console.log (isFizzBuzz_x_y(x,3,7))
+// console.log (isFizzBuzz_range(1,100,3,7))
+
+
+
+function makeFizzBuzzFunc (num1,num2){
+  var extraStep = function(x){
+    if ( x % (num1 * num2) === 0){
+      return "FIZZBUZZ";
+    } else if ( x % num2 === 0){
+      return "BUZZ";
+    } else if ( x % num1 === 0){
+      return "FIZZ";
+    } else {
+      return x;
+    }
+  }
+  return extraStep
 }
+
+for (x = 1; x <= 100; x++) {
+    var fizzBuzz_5_7 = makeFizzBuzzFunc (5, 7);
+    console.log ( fizzBuzz_5_7(x) );
+ }
